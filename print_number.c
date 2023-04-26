57c9e1c853b87189e8455b119940770aaab7334a
Omar atef <change0m0@gmail.com>
2023-04-26 05:16:22 +0200

    update

--- print_number.c
+++ print_number.c
@@ -26,9 +26,9 @@ int _strlen(char *s)
 	return (i);
 }
 /**
- * print_numer - func
+ * print_number - func
  * @str: sd
- * @parems: pase
+ * @params: pase
  *
  * Return: char
  */
@@ -71,11 +71,53 @@ int print_number_right_shift(char *str, params_t *params)
 		str++;
 	else
 		neg = 0;
-	if ((params->plus_flag && !neg2) || 
+	if ((params->plus_flag && !neg2) ||
 			(!params->plus_flag && params->space_flag && !neg2))
 		i++;
 	if (neg && pad_char == '0')
 		n += _putchar('-');
-	if (params->plus_flag && !neg2 && pad_char == '0' && !params-.unsign)
+	if (params->plus_flag && !neg2 && pad_char == '0' && !params->unsign)
 		n += _putchar('+');
-	else if
+	else if (!params->plus_flag && params->space_flag && !neg2 &&
+			!params->unsign && params->zero_flag)
+		n += _putchar(' ');
+	while (i++ < params->width)
+		n += _putchar(pad_char);
+	if (neg && pad_char == ' ')
+		n += _putchar('-');
+	if (params->plus_flag && !neg2 && pad_char == ' ' && !params->unsign)
+		n += _putchar('+');
+	else if (!params->plus_flag && params->space_flag && !neg2 &&
+			!params->unsign && !params->zero_flag)
+		n += _putchar(' ');
+	n += _puts(str);
+	return (n);
+}
+/**
+ * print_number_left_shift - func
+ * @str: sd
+ * @params: params
+ *
+ * Return: char
+ */
+int print_number_left_shift(char *str, params_t *params)
+{
+	unsigned int n = 0, neg, neg2, i = _strlen(str);
+	char pad_char = ' ';
+
+	if (params->zero_flag && !params->minus_flag)
+		pad_char = '0';
+	neg = neg2 = (!params->unsign && *str == '-');
+	if (neg && i < params->width && pad_char == '0' && !params->minus_flag)
+		str++;
+	else
+		neg = 0;
+	if (params->plus_flag && !neg2 && !params->unsign)
+		n += _putchar('+'), i++;
+	else if (params->space_flag && !neg2 && !params->unsign)
+		n += _putchar(' '), i++;
+	n += _puts(str);
+	while (i++ < params->width)
+		n += _putchar(pad_char);
+	return (n);
+}
